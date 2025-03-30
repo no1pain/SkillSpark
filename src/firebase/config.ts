c11c4a54fc5dd9fd52293616b306113f2aeeac3c
Oleksandr <kazansasha04@gmail.com>
2025-03-30 23:02:19 +0300

    fixed auth

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyALy4ZjD_DOpU3FFQwxl3akLjtCX0JdTcQ",
   authDomain: "skillspark-a4a76.firebaseapp.com",
   projectId: "skillspark-a4a76",
-  storageBucket: "skillspark-a4a76.firebasestorage.app",
+  storageBucket: "skillspark-a4a76.appspot.com",
   messagingSenderId: "973879636137",
   appId: "1:973879636137:web:361bc439d6d120641bf9b8",
   measurementId: "G-METH54G93R"
